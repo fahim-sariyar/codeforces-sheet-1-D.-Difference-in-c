0bd2fae5f6341ccb6788bb6c8a784a58ab0497cf
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2024-11-15 00:29:25 +0600

    codeforces-sheet-1-C.cpp

--- codeforces-sheet-1-C.cpp
+++ codeforces-sheet-1-C.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int X;
+    long long Y;
+    cin >> X >> Y;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;    
+    return 0;
+}
